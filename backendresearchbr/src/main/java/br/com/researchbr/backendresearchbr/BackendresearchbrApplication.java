95d6ad2fcdbbb58321b7d4327b2042bd05d67e79
tiagofalcon <tiago.falcon@dbccompany.com.br>
2020-12-01 11:22:34 -0300

    feat(backendreasearchbr): create initial spring project with spring web data and security

---
+++ backendresearchbr/src/main/java/br/com/researchbr/backendresearchbr/BackendresearchbrApplication.java
@@ -0,0 +1,13 @@
+package br.com.researchbr.backendresearchbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendresearchbrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendresearchbrApplication.class, args);
+	}
+
+}
---
+++ backendresearchbr/src/test/java/br/com/researchbr/backendresearchbr/BackendresearchbrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.researchbr.backendresearchbr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendresearchbrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
